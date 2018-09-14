2c1214e241b78c3bfdb1a9a86cd4b9cb5cbdbf62
rpi_sainquake <sainquake@gmail.com>
2018-09-14 14:51:07 +0300

    get/setTemp add

--- OpenThermHat.py
+++ OpenThermHat.py
@@ -15,32 +15,32 @@ class OpenThermHat:
 		# Disable "This channel is already in use" warnings
 		GPIO.setwarnings(False)
 		# Setup MCU POWER UP
-        GPIO.setup(17, GPIO.OUT)
-        GPIO.output(17, True)
-        #LEDS
-        GPIO.setup(4, GPIO.OUT)
-        GPIO.output(4, True)
+	        GPIO.setup(17, GPIO.OUT)
+        	GPIO.output(17, True)
+        	#LEDS
+        	GPIO.setup(4, GPIO.OUT)
+        	GPIO.output(4, True)
 		
-        GPIO.setup(27, GPIO.OUT)
-        GPIO.output(27, True)
+        	GPIO.setup(27, GPIO.OUT)
+        	GPIO.output(27, True)
 		
-        GPIO.setup(6, GPIO.OUT)
-        GPIO.output(6, True)
+        	GPIO.setup(6, GPIO.OUT)
+        	GPIO.output(6, True)
 		
-        self.ser = serial.Serial ("/dev/ttyAMA0")    #Open named port
-        self.ser.baudrate = 115200                     #Set baud rate to 9600
-        self.ser.timeout = 1
+        	self.ser = serial.Serial ("/dev/ttyAMA0")    #Open named port
+        	self.ser.baudrate = 115200                     #Set baud rate to 9600
+        	self.ser.timeout = 1
 	def sendReceive(self,ad0,ad1,data0,data1):
-        # address0 address1 data0 data1
-        values = bytearray([ad0, ad1, data0, data1])
-        self.ser.write(values)
-        time.sleep(0.1)
-        data = self.ser.read(4)
-        print 'data=' , ":".join("{:02x}".format(ord(c)) for c in data)
-        return data
-	def setTemp(self,temp)
+        	# address0 address1 data0 data1
+        	values = bytearray([ad0, ad1, data0, data1])
+        	self.ser.write(values)
+        	time.sleep(0.1)
+        	data = self.ser.read(4)
+        	print 'data=' , ":".join("{:02x}".format(ord(c)) for c in data)
+        	return data
+	def setTemp(self,temp):
 		return self.sendReceive(10,0,0,temp)
-	def getTemp(self)
+	def getTemp(self):
 		return self.sendReceive(11,0,0,0)
 
 oth = OpenThermHat()
