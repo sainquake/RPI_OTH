196c91ee5d52f1c5bca1002e3c624ae1a8f86686
constantin <sainquake@gmail.com>
2018-10-06 23:21:23 +0400

    server

--- server.py
+++ server.py
@@ -3,9 +3,20 @@
 from http.server import BaseHTTPRequestHandler,HTTPServer
 import shutil
 import sys
+from OpenThermHat import OpenThermHat
+import time
+
+oth = OpenThermHat()
+#oth.resetMCU()
 
 PORT_NUMBER = 8080
 
+
+echo = oth.sendReceive(OpenThermHat.RPi_ECHO_UART_ADDRESS,0,4,5)
+if (echo>>16)&0xFFFF!=0x0504:
+	oth.resetMCU()
+	print("RESET MCU")
+	time.sleep(5)
 #This class will handles any incoming request from
 #the browser 
 class myHandler(BaseHTTPRequestHandler):
@@ -22,9 +33,16 @@ class myHandler(BaseHTTPRequestHandler):
 		self.wfile.write( f.read().encode() )
 		f.close()
 		if self.path=="/ot":
-			self.wfile.write(("<div class='w3-cell-row w3-container'><div class='w3-cell'>").encode() )
-			self.wfile.write(("<p>temp: 88</p>").encode() )
-			self.wfile.write(("</div></div><hr>").encode() )
+			oth.getOTStatus()
+			self.wfile.write(("<div class='w3-cell-row w3-container'><div class='w3-cell'><p>'ot.timeout=\t'"+str(oth.otData.otTimeout)+"</p></div></div><hr>").encode() )
+			self.wfile.write(("<div class='w3-cell-row w3-container'><div class='w3-cell'><p>'ot.otIndex=\t'"+str(oth.otData.otIndex)+"</p></div></div><hr>").encode() )
+			self.wfile.write(("<div class='w3-cell-row w3-container'><div class='w3-cell'><p>'ot.otBusy=\t'"+str(oth.otData.otBusy)+"</p></div></div><hr>").encode() )
+			self.wfile.write(("<div class='w3-cell-row w3-container'><div class='w3-cell'><p>'ot.otComplete=\t'"+str(oth.otData.otComplete)+"</p></div></div><hr>").encode() )
+			self.wfile.write(("<div class='w3-cell-row w3-container'><div class='w3-cell'><p>'ot.otFrameSendedAndStartWaitingACK=\t'"+str(oth.otData.otFrameSendedAndStartWaitingACK)+"</p></div></div><hr>").encode() )
+			self.wfile.write(("<div class='w3-cell-row w3-container'><div class='w3-cell'><p>'ot.otReadingResponse=\t'"+str(oth.otData.otReadingResponse)+"</p></div></div><hr>").encode() )
+			self.wfile.write(("<div class='w3-cell-row w3-container'><div class='w3-cell'><p>'ot.otSpecialRequest=\t'"+str(oth.otData.otSpecialRequest)+"</p></div></div><hr>").encode() )
+			self.wfile.write(("<div class='w3-cell-row w3-container'><div class='w3-cell'><p>'ot.otSpecialRequestComplete=\t'"+str(oth.otData.otSpecialRequestComplete)+"</p></div></div><hr>").encode() )
+			self.wfile.write(("<div class='w3-cell-row w3-container'><div class='w3-cell'><p>'ot.otNoResponse=\t'"+str(oth.otData.otNoResponse)+"</p></div></div><hr>").encode() )	
 		f = open('footer.html', 'r')
 		self.wfile.write( f.read().encode() )
 		f.close()
